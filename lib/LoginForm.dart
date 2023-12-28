e0bef225e377de50dc2fa881c929fdf0ae05b8a4
1u1uwa <ayawehbeh1@gmail.com>
2023-12-28 03:17:57 +0200

    container

--- lib/LoginForm.dart
+++ lib/LoginForm.dart
@@ -15,29 +15,45 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextField(
-          controller: _usernameController,
-          decoration: InputDecoration(labelText: 'Username'),
+    return Container(
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(10.0),
         ),
-        TextField(
-          controller: _passwordController,
-          obscureText: true,
-          decoration: InputDecoration(labelText: 'Password'),
-        ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomeScreen()),
-            );
-          },
-          child: Text('Login'),
-        ),
-      ],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Login",style: TextStyle(fontSize: 25 , fontWeight: FontWeight.bold),),
+          TextField(
+            controller: _usernameController,
+            decoration:
+            InputDecoration(
+              labelText: 'Username',
+
+            ),
+          ),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: InputDecoration(labelText: 'Password'),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen()),
+              );
+            },
+            child: Text('Login'),
+            style:
+            ButtonStyle(
+              
+            ),
+          ),
+        ],
+    )
     );
   }
 }
