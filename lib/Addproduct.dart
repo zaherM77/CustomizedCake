4d64857d4c25e8f35a4579a0620d17bfcf68732a
1u1uwa <ayawehbeh1@gmail.com>
2024-01-02 00:46:47 +0200

    update

--- lib/Addproduct.dart
+++ lib/Addproduct.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+
 class AddProduct extends StatefulWidget {
   const AddProduct({Key? key}) : super(key: key);
 
