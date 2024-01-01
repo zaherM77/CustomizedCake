87ed7430dc47800b481f3867fd07383e727e65c6
1u1uwa <ayawehbeh1@gmail.com>
2024-01-02 01:05:42 +0200

    update

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,15 @@
 import FlutterMacOS
 import Foundation
 
+<<<<<<< Updated upstream
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+=======
+import file_selector_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+>>>>>>> Stashed changes
 }
